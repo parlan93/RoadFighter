f84fab44a16bb92af1f5054886f1137a8d64fc5f
joanow <asia.nowakowska.94@gmail.com>
2017-09-17 16:03:07 +0200

    Enemy Cars

---
+++ Assets/Scripts/Obstacles/EnemyBlueCar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBlueCar : EnemyVehicle
+{
+
+    protected override void Start()
+    {
+        StartComponent();
+    }
+
+    protected override void Update()
+    {
+        UpdateComponent();
+    }
+
+    protected override void OnCollisionEnter2D(Collision2D col)
+    {
+        OnCollisionEnter2DComponent(col);
+    }
+
+    protected override void OnTriggerEnter2D(Collider2D col)
+    {
+        OnTriggerEnter2DComponent(col);
+    }
+
+}
